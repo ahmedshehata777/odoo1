136134b1e1ee3cd17ede3c8bbd655e8665ba5429
Ahmed shehata <121793892+ahmedshehata777@users.noreply.github.com>
2024-03-11 10:10:52 +0200

    Add files via upload

---
+++ to_do_app/__mainfest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "To do",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Ahmed shehata",
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menus.xml',
+        'views/todo.xml',
+    ],
+}
